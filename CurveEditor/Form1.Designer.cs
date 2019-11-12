8f292590efe959e4b5b3ed6be44c082c037e51fc
ootake305 <34718190+ootake305@users.noreply.github.com>
2019-11-13 07:26:51 +0900

    Add files via upload

---
+++ CurveEditor/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CurveEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(708, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(23, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 1080);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.TestPaint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(708, 338);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "縮小";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 484);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CurveEditor/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Windows.Input;
+
+
+namespace CurveEditor
+{
+    public partial class Form1 : Form
+    {
+        const float cpSize = 15;
+        Point[] Points;
+        Point[] Points2;
+        Point[] Points3;
+        Point point1 = new Point(50, 300);
+        Point point2 = new Point(150, 150);
+        Point point3 = new Point(300, 400);
+        Point point4 = new Point(450, 150);
+        Point point5 = new Point(550, 250);
+        Pen pen = new Pen(Color.White, 1);
+        Brush brush = new SolidBrush(Color.FromArgb(160, 255, 0, 0));
+        int moveIndex = 0;
+        bool moveFlag = false;
+        GraphicsPath path = new GraphicsPath();
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "DrawBezier";
+            Points = new Point[] { point1, point2 , point3, point1 };
+            Points2 = new Point[] { point2, point3, point4, point5 };
+            Points3 = new Point[] { point3, point4 };
+            InitPointLocationLabel();
+            SetStyle(
+    ControlStyles.DoubleBuffer |
+    ControlStyles.UserPaint |
+    ControlStyles.AllPaintingInWmPaint, true);
+           // path.AddCurve(Points);
+            //path.AddCurve(Points2);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+           /* Graphics g = e.Graphics;
+            // g.DrawCurve(pen, Points,0.5f);
+            // g.DrawCurve(pen, Points2, 0.0f);
+            //  g.DrawLine(pen, Points[0], Points[1]);
+            // g.DrawLine(pen, Points[3], Points[2]);
+        
+            g.Clear(Color.White);
+            path.Reset();
+            path.AddCurve(Points,1,1,1);
+         //  path.AddCurve(Points3,0.5f);
+           // path.AddCurve(Points3);
+            // path.PathPoints[0] = point1;
+            for (int i = 0; i < Points.Length; i++)
+            {
+                e.Graphics.FillRectangle(brush, Points[i].X - cpSize / 2, Points[i].Y - cpSize / 2, cpSize, cpSize);
+            }
+            for (int i = 0; i < Points2.Length; i++)
+            {
+                e.Graphics.FillRectangle(brush, Points2[i].X - cpSize / 2, Points2[i].Y - cpSize / 2, cpSize, cpSize);
+            }
+            e.Graphics.DrawPath(pen, path);*/
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < Points.Length; i++)
+            {
+                if (e.X >= Points[i].X - cpSize / 2 && e.X < Points[i].X + cpSize / 2)
+                {
+                    if (e.Y >= Points[i].Y - cpSize / 2 && e.Y < Points[i].Y + cpSize / 2)
+                    {
+                        moveIndex = i;
+                        moveFlag = true;
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moveFlag = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moveFlag)
+            {
+                if (e.X < 1000)
+                {
+                    Points[moveIndex].X = e.X;
+                }
+                else
+                {
+                    Points[moveIndex].X = 1000;
+                }
+
+                Points[moveIndex].Y = e.Y;
+
+                Control cx = Controls[moveIndex + "X"];
+                cx.Text = Points[moveIndex].X.ToString();
+                Control cy = Controls[moveIndex + "Y"];
+                cy.Text = Points[moveIndex].Y.ToString();
+
+                Invalidate();
+                Refresh();
+            }
+          
+        }
+        private void InitPointLocationLabel()
+        {
+            for (int i = 0; i < Points.Length; i++)
+            {
+                Label l = new Label();
+                l.Location = new Point(100, 20 * i + 400);
+                l.Size = new Size(64, 20);
+                l.Text = "Point" + i;
+                Controls.Add(l);
+                Label l2 = new Label();
+                l2.Name = i + "X";
+                l2.Text = Points[i].X.ToString();
+                l2.Location = new Point(164, 20 * i + 400);
+                l2.Size = new Size(64, 20);
+                Controls.Add(l2);
+                Label l3 = new Label();
+                l3.Name = i + "Y";
+                l3.Text = Points[i].X.ToString();
+                l3.Location = new Point(228, 20 * i + 400);
+                l3.Size = new Size(64, 20);
+                Controls.Add(l3);
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TestPaint(object sender, PaintEventArgs e)
+        {
+       
+            Graphics g = e.Graphics;
+            // g.DrawCurve(pen, Points,0.5f);
+            // g.DrawCurve(pen, Points2, 0.0f);
+            //  g.DrawLine(pen, Points[0], Points[1]);
+            // g.DrawLine(pen, Points[3], Points[2]);
+
+            g.Clear(Color.FromArgb(20, 255, 255, 255));
+            path.Reset();
+            path.AddCurve(Points, 1, 1, 1);
+            path.AddCurve(Points2, 1, 1, 1);
+            //  path.AddCurve(Points3,0.5f);
+            // path.AddCurve(Points3);
+            // path.PathPoints[0] = point1;
+            for (int i = 0; i < Points.Length; i++)
+            {
+                e.Graphics.FillRectangle(brush, Points[i].X - cpSize / 2, Points[i].Y - cpSize / 2, cpSize, cpSize);
+            }
+            for (int i = 0; i < Points2.Length; i++)
+            {
+                e.Graphics.FillRectangle(brush, Points2[i].X - cpSize / 2, Points2[i].Y - cpSize / 2, cpSize, cpSize);
+            }
+            e.Graphics.DrawPath(pen, path);
+ 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
